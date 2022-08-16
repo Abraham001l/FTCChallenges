079ab8db5f145b0b320e718cca362a7e5405de93
Abraham <abraham51lbucio@gmail.com>
2022-08-16 09:53:57 -0500

    tracking keys successfully

--- DcMotor.java
+++ DcMotor.java
@@ -1,44 +1,44 @@
-import java.lang.Math;
+// import java.lang.Math;
 
-public class DcMotor {
+// public class DcMotor {
 
-  public static enum Direction { FORWARD, REVERSE;
+// public static enum Direction { FORWARD, REVERSE;
 
-  }
+// }
 
-  public static enum ZeroPowerBehavior {
-    UNKNOWN,
-    FLOAT,
-    BRAKE;
-  }
-  
-  private boolean polarization = true;
-  private DcMotor.Direction direction = DcMotor.Direction.FORWARD;
-  private double power = 0;
-  private DcMotor.ZeroPowerBehavior brakeMode = DcMotor.ZeroPowerBehavior.
+// public static enum ZeroPowerBehavior {
+// UNKNOWN,
+// FLOAT,
+// BRAKE;
+// }
 
-  public DcMotor(){
-    polarization = (Math.random()>0.5);
-  }
+// private boolean polarization = true;
+// private DcMotor.Direction direction = DcMotor.Direction.FORWARD;
+// private double power = 0;
+// private DcMotor.ZeroPowerBehavior brakeMode = DcMotor.ZeroPowerBehavior.
 
-  public void setDirection(DcMotor.Direction setDirection) {
-    direction = setDirection;
-  }
-  public DcMotor.Direction getDirection() {
-    return direction;
-  }
+// public DcMotor(){
+// polarization = (Math.random()>0.5);
+// }
 
-  public void setZeroPowerBehavior(DcMotor.ZeroPowerBehavior brakeState) {
-    brakeOn = brakeState;
-  }
-  public DcMotor.ZeroPowerBehavior getZeroPowerBehavior() {
-    return brakeOn;
-  }
+// public void setDirection(DcMotor.Direction setDirection) {
+// direction = setDirection;
+// }
+// public DcMotor.Direction getDirection() {
+// return direction;
+// }
 
-  public void setPower(double newPower) {
-    power = newPower; 
-  }
-  public double getPower() {
-    return power;
-  }
-}
+// public void setZeroPowerBehavior(DcMotor.ZeroPowerBehavior brakeState) {
+// brakeOn = brakeState;
+// }
+// public DcMotor.ZeroPowerBehavior getZeroPowerBehavior() {
+// return brakeOn;
+// }
+
+// public void setPower(double newPower) {
+// power = newPower;
+// }
+// public double getPower() {
+// return power;
+// }
+// }
