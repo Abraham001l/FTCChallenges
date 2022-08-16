c5b99e8867ef921d817d3a55c096c563f9c2a834
DrIronfist <reetsinha2006@gmail.com>
2022-08-15 23:28:53 -0500

    added opmode

---
+++ OpMode.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class OpMode {
+  public void init() {
+    // your code
+  }
+
+  public void init_loop() {
+    // your code
+  }
+
+  public void start() {
+    // runtime code
+  }
+
+  public void loop() {
+    // your code
+  }
+
+  public void stop() {
+    // your code
+  }
+}
